d93b24c5e506658cdeb80f3e2e33bbe64ff1ebab
Vladimir <ustinov5690@mail.ru>
2025-02-26 11:33:45 +0600

    Добавил обработчик get_data

--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def get_data(request):
+    return JsonResponse({"status": "ok"}, status=200)
+
